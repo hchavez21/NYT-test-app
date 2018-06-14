47438b9f77835826bfe4df2fac763c2f928c6661
Hugo <hchavez626@gmail.com>
2018-06-13 20:43:41 -0500

    api key and ajax

--- java.js
+++ java.js
@@ -1,4 +1,9 @@
-var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+var term= $(this).attr("data-term");
+var queryURL = "http://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + search + "&page=2&sort=oldest&api-key=b5088b114c8246f19bedeeddb89b295a";
 
-  'api-key': "b5088b114c8246f19bedeeddb89b295a",
-  
+ $.ajax({
+    url: queryURL,
+    method: "GET"
+ }).then(function(response) {
+
+ })
