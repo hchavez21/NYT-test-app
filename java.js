d0bf96b8d8c447835e226f260c0c287be1330561
rspears417 <racheal.spears417@gmail.com>
2018-06-13 20:31:16 -0500

    added branch

--- java.js
+++ java.js
@@ -1,4 +1,26 @@
 var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
 
   'api-key': "b5088b114c8246f19bedeeddb89b295a",
-  
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  })
+    .then(function(response) {
+      var results = response.data;
+
+      for (var i = 0; i < results.length; i++) {
+        var gifDiv = $("<div class='item'>");
+
+        var rating = results[i].rating;
+
+        var p = $("<p>").text("Rating: " + rating);
+
+        var personImage = $("<img>");
+        personImage.attr("src", results[i].images.fixed_height.url);
+
+        gifDiv.prepend(p);
+        gifDiv.prepend(personImage);
+
+        $("#gifs-appear-here").prepend(gifDiv);
+      }
+    });
